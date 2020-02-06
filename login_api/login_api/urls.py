d8445e33481154904d0483c0ef92ced494e2e858
Krishna Verma <krishna.verma@yan3k.com>
2020-02-06 16:28:08 +0800

    Adding Oauth TO the App

--- login_api/login_api/urls.py
+++ login_api/login_api/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework.authtoken import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('', include('login.urls')),
+    url('api-token-oauth', views.obtain_auth_token, name='api-token-auth')
 ]
